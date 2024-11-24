29b8535d3b9882f9f260b73b7f8694809982960a
Navnath Gutte <60350293+Navnath3864@users.noreply.github.com>
2024-11-24 14:50:51 +0530

    Update ProductmanagementsystemApplication.java
    add print statement in main method

--- productmanagementsystem/src/main/java/com/org/product/ProductmanagementsystemApplication.java
+++ productmanagementsystem/src/main/java/com/org/product/ProductmanagementsystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductmanagementsystemApplication {
 
 	public static void main(String[] args) {
+		System.out.println("This is Project Management System");
 		SpringApplication.run(ProductmanagementsystemApplication.class, args);
 	}
 
